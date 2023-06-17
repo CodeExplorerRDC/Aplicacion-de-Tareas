f8dca01ce90b3a8cab197ff761b1de7698aa55c0
Robert <wolfswag96yolo@gmail.com>
2023-06-17 14:35:43 -0600

    Esta es la versión Original de mi Aplicación de Tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Logo from './imagenes/logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={Logo}/>
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas/>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
